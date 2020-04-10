cdbaa9f8505ea6b3bbe8c4808cc52f593582ebe7
antdp425 <antdp425@aol.com>
2020-04-10 19:28:03 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,59 @@
 require 'open-uri'
 require 'pry'
 
+#Using this to simply scrape then pass along to student
 class Scraper
 
+  #passes in a url tht we can scrape from w/ nokogiri
   def self.scrape_index_page(index_url)
+    students = [] #sets array to be returned later
+
+    #takes in html of passed in URL
+    doc = Nokogiri::HTML(open(index_url))
+    #gets each student card so that we can pull from it
+    student = doc.css("div.student-card")
     
+    #for each student card, pass in name / location / profile_url into the array of hashes
+    student.each do |s|
+    students << {
+        :name => s.css(".card-text-container h4.student-name").children[0].text,
+        :location => s.css(".card-text-container p.student-location").children[0].text,
+        :profile_url => s.css("a").attribute("href").value }
+    end
+    students
   end
 
+  #passes in url that we can scrape 
   def self.scrape_profile_page(profile_url)
-    
+
+  #gets html of url
+  doc = Nokogiri::HTML(open(profile_url))
+
+  #gets all links
+  links = doc.css('a')
+  #gets all uniq hrefs from links and deletes if blank or if does not contain .com
+  hrefs = links.map{|link| link.attribute('href').to_s}.uniq.sort.delete_if{|href| href.empty? || !href.include?(".com")}
+  
+  #creates profile hash
+  profile = {}
+
+  #assigns profile quote + bio text to hash
+  profile[:profile_quote] = doc.css("div.profile-quote").text
+  profile[:bio] = doc.css("div.description-holder p").text
+
+  #goes through each link and adds k/v when necessary
+  hrefs.each do |h|
+    if h.include?("twitter")
+      profile[:twitter] = h
+    elsif h.include?("linkedin")
+      profile[:linkedin] = h
+    elsif h.include?("github")
+      profile[:github] = h
+    else  
+      profile[:blog] = h
+    end
+    end
+    profile
   end
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -4,20 +4,29 @@ class Student
 
   @@all = []
 
+  #creates new student via hash thae we passed in via scraper
   def initialize(student_hash)
-    
+    #for each student in hash, create a k/v pair via mass asign (.send)
+    student_hash.each {|k,v| self.send(("#{k}="), v)}
+    self.save
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each {|s| s = self.new(s)}
   end
 
+  #adds additional attributes to student instance via mass assign (.send)
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each {|k,v| self.send(("#{k}="), v)}
   end
 
   def self.all
-    
+    @@all
   end
+
+  def save
+    @@all << self
+  end
+
 end
 
